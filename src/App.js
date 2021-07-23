f1ba89f7dd106ff5a9bc891e793681eb98a31156
Igoris Ivanovas <igoris.x@inbox.lt>
2021-07-23 20:42:12 +0300

    day3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { computeHeadingLevel } from '@testing-library/react';
+
 import React, { useState, useRef } from 'react';
 import TodoList from './TodoList';
+import uuidv4 from 'uuid/v4'
 
 
 
@@ -13,7 +14,7 @@ function App() {
     const name = todoNameRef.current.value
     if (name === '') return
     setTodos(prevTods => {
-      return [...prevTods, { id: 1, name: name, complete: false }]
+      return [...prevTods, { id: uuidv4(), name: name, complete: false }]
     })
 
   }
