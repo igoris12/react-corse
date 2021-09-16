87d2f3793e62a5919e3dd887c94c28e42344f23d
Igoris Ivanovas <igoris.x@inbox.lt>
2021-09-16 16:22:53 +0300

    cleaning

--- src/Componets/App.jsx
+++ src/Componets/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Mygtukas from './Mygtukas';
+
 function App() {
     return (
-        <>
-           <Mygtukas/> 
-        </>
-
+        <div>
+            asgasdfs
+        </div>
     )
 }
 
