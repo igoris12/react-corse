07306943b7ba39547dd6b4d5c2beb1c523486e88
Igoris Ivanovas <igoris.x@inbox.lt>
2021-09-18 19:29:35 +0300

    input and state bug needs fix

---
+++ src/Componets/Animal.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Animal extends Component {
+  render() {
+    return (
+      <div className="animal">
+        <span>colro</span>
+      </div>
+    );
+  }
+}
+
+export default Animal;
--- src/Componets/App.jsx
+++ src/Componets/App.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from 'react';
+import Animal from './Animal';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      animals: [],
+      input: '',
+    };
+  }
+
+  inputHandler = (e) => {
+    this.setState({
+      input: e.target.value,
+    });
+  };
+
+  addAnimal = (a) => {
+    const animal = { color: this.state.input, animal: a };
+    const animals = this.state.animals;
+    animals.push(animal);
+    this.setState = {
+      animals: animals,
+    };
+  };
+
+  render() {
     return (
-        <div>
-            asgasdfs
-        </div>
-    )
+      <>
+        {this.state.animals.map((e) => (
+          <Animal />
+        ))}
+        <input
+          type="text"
+          value={this.state.input}
+          onChange={this.inputHandler}
+        />
+        <button className="but" onClick={() => this.addAnimal('cow')}>
+          Cow
+        </button>
+        <button className="but" onClick={() => this.addAnimal('sheep')}>
+          Sheep
+        </button>
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
