55868437f4b4df897650db61b4b4077fe38fa555
Igoris Ivanovas <igoris.x@inbox.lt>
2021-09-19 20:06:35 +0300

    local storege and bug fix

--- src/Componets/Animal.jsx
+++ src/Componets/Animal.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 export class Animal extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editInput: '',
+    };
+  }
   render() {
     return (
-      <div className="animal">
-        <span>colro</span>
+      <div
+        className="animal"
+        style={{
+          backgroundColor: this.props.color,
+          borderRadius: this.props.animal == 'cow' ? '50%' : '5px',
+        }}
+      >
+        <div>
+          <span>{this.props.color}</span>
+        </div>
+        <div className="insideForm">
+          <input type="text" />
+          <button className="but">Edit</button>
+          <button className="but">Delete</button>
+        </div>
       </div>
     );
   }
--- src/Componets/App.jsx
+++ src/Componets/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Animal from './Animal';
+import getId from '../Shared/getId';
 
 class App extends React.Component {
   constructor() {
@@ -10,26 +11,37 @@ class App extends React.Component {
     };
   }
 
+  addAnimal = (a) => {
+    const animal = { id: getId(), color: this.state.input, animal: a };
+    const animals = this.state.animals.slice();
+    animals.push(animal);
+    this.setState({
+      animals: animals,
+    });
+    localStorage.setItem('allAnimals', JSON.stringify(animals));
+  };
+
   inputHandler = (e) => {
     this.setState({
       input: e.target.value,
     });
   };
 
-  addAnimal = (a) => {
-    const animal = { color: this.state.input, animal: a };
-    const animals = this.state.animals;
-    animals.push(animal);
-    this.setState = {
+  componentDidMount() {
+    const animals = JSON.parse(localStorage.getItem('allAnimals'));
+    if (null == animals) {
+      return;
+    }
+    this.setState({
       animals: animals,
-    };
-  };
+    });
+  }
 
   render() {
     return (
       <>
-        {this.state.animals.map((e) => (
-          <Animal />
+        {this.state.animals.map((a) => (
+          <Animal key={a.id} color={a.color} animal={a.animal} />
         ))}
         <input
           type="text"
