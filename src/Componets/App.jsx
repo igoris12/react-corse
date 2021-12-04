b2723786273656708fa70f1ab670c65979ded305
Igoris Ivanovas <igoris.x@inbox.lt>
2021-12-04 20:08:47 +0200

    set useReducer and useContext

--- src/Componets/App.jsx
+++ src/Componets/App.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import UseReduserCom from './reactHooks/UseReducer/UseReduserCom';
 import UseReduserCom2 from './reactHooks/UseReducer/UseReduserCom2';
 import UseReduserCom3 from './reactHooks/UseReducer/UseReduserCom3';
 import ComponentA from './reactHooks/useRandUseC/ComponentA';
+import { CountState } from '../Data/context';
+
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'restart':
+      return 0;
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, 0);
   return (
-    <div>
+    <CountState.Provider>
       {/* <UseReduserCom /> */}
       {/* <UseReduserCom2 /> */}
       {/* <UseReduserCom3 /> */}
       <ComponentA />
-    </div>
+    </CountState.Provider>
   );
 }
 
--- src/Data/context.jsx
+++ src/Data/context.jsx
@@ -1,3 +1,4 @@
 import { createContext } from 'react';
 export const Context = createContext('Default Value');
 export const workContext = createContext('Default Value');
+export const CountState = createContext(null);
