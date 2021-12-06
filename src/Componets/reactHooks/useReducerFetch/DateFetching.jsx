a0ddf03e7d8796771f344b8048425c404f5c9607
Igoris Ivanovas <igoris.x@inbox.lt>
2021-12-06 21:58:29 +0200

    set useReducer

--- src/Componets/reactHooks/useReducerFetch/DateFetching.jsx
+++ src/Componets/reactHooks/useReducerFetch/DateFetching.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
+
+const initialValue = {
+  loading: true,
+  post: {},
+  error: '',
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS':
+      return {
+        loading: false,
+        post: action.data,
+        error: '',
+      };
+    case 'FETCH_ERROR':
+      return {
+        loading: false,
+        post: {},
+        error: 'Something is not working how it was planned.',
+      };
+
+    default:
+      return state;
+  }
+};
 
 const DateFetching = () => {
-  return <div>data</div>;
+  const [state, dispatch] = useReducer(reducer, initialValue);
+  return (
+    <div>
+      {state.loading ? 'Loding...' : state.post.titel}
+      {state.error ? state.error : null}
+    </div>
+  );
 };
 
 export default DateFetching;
