8c8ab7df136cc83760da498618277670de3cadbf
Igoris Ivanovas <igoris.x@inbox.lt>
2021-12-12 17:59:21 +0200

    style done

--- src/Componets/App.jsx
+++ src/Componets/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <div className="contaner">
+    <div className="dark">
+      <button className="btn-theme">Theme</button>
+
+      <div className="contaner ">
         <h4>count - 0</h4>
-        <button className="btn-add">Increment</button>
-        <button className="btn-sub">Decrement</button>
-        <button className="btn-async">Async</button>
+        <div className="btn-box">
+          <button className="btn-add">Increment</button>
+          <button className="btn-sub">Decrement</button>
+          <button className="btn-async">Async</button>
+        </div>
       </div>
     </div>
   );
