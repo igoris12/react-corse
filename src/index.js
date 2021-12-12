851c94fa78d70221efd33f14b6cc8a3483a214b6
Igoris Ivanovas <igoris.x@inbox.lt>
2021-12-12 17:39:38 +0200

    style file set

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './Componets/App'
+import App from './Componets/App';
+import './css/reduxStudy.scss';
 
 
 
