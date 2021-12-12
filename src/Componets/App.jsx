a4de787b7fdb764fce2ebba950950ff3ed3a50c3
Igoris Ivanovas <igoris.x@inbox.lt>
2021-12-12 17:36:53 +0200

    Layout setup

--- src/Componets/App.jsx
+++ src/Componets/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const App = () => {
-  return <div>redux shit</div>;
+  return (
+    <div>
+      <div className="contaner">
+        <h4>count - 0</h4>
+        <button className="btn-add">Increment</button>
+        <button className="btn-sub">Decrement</button>
+        <button className="btn-async">Async</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
