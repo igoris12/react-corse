c36514c75a230f6c6ea0c103eeea80fd812ebd25
Igoris Ivanovas <igoris.x@inbox.lt>
2021-12-16 18:35:27 +0200

    Rework deleted useless state

--- src/Componets/App.jsx
+++ src/Componets/App.jsx
@@ -8,8 +8,12 @@ import Logo from './Logo';
 const App = () => {
   const [registration, setRegistration] = useState(false);
   const [from, setForm] = useState(false);
-  const [loged, setLoged] = useState(false);
-  const [user, setUser] = useState({ username: '', password: '' });
+
+  const [user, setUser] = useState({
+    username: '',
+    password: '',
+    loged: false,
+  });
 
   console.log(user);
   const goRegistrate = () => {
@@ -19,32 +23,32 @@ const App = () => {
 
   const getRegistration = (e) => {
     e.preventDefault();
-    // console.log(e.target[0].value);
-    // console.log(e.target[1].value);
-    // console.log(e);
     if (e.target[2].value === e.target[1].value) {
       setUser({
         username: e.target[0].value,
         password: e.target[1].value,
+        loged: true,
       });
-      setLoged(true);
     } else {
       alert('Passwors must match!');
     }
   };
 
   const logout = () => {
-    setLoged(false);
+    setUser({
+      ...user,
+      loged: false,
+    });
   };
 
   return (
     <div>
       <Logo />
-      {!registration && !loged && (
+      {!registration && !user.loged && (
         <LandingPage registrationHandler={goRegistrate} />
       )}
-      {from && !loged && <Form getRegistration={getRegistration} />}
-      {loged && <HomePage logout={logout} />}
+      {from && !user.loged && <Form getRegistration={getRegistration} />}
+      {user.loged && <HomePage logout={logout} />}
     </div>
   );
 };
