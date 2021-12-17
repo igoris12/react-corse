2d64560c0fef32862543720eae76a2fb29b1c08e
Igoris Ivanovas <igoris.x@inbox.lt>
2021-12-17 19:34:44 +0200

    put info in localStorage

--- src/Componets/App.jsx
+++ src/Componets/App.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import Counter1 from './Counter1';
+import React, { useState, useEffect } from 'react';
 import Form from './Form';
 import HomePage from './HomePage';
 import LandingPage from './LandingPage';
@@ -8,15 +7,12 @@ import Logo from './Logo';
 const App = () => {
   const [registration, setRegistration] = useState(false);
   const [from, setForm] = useState(false);
-
   const [user, setUser] = useState({
     username: '',
     password: '',
     email: '',
     loged: false,
   });
-
-  console.log(user);
 
   const goRegistrate = () => {
     setRegistration(true);
@@ -31,25 +27,35 @@ const App = () => {
         'Content-Type': 'application/json',
       },
     });
-    return response;
-  };
 
-  sendData('http://jsonplaceholder.typicode.com/users', { A: 3, b: 3 })
-    .then((response) => {
-      console.log(response.status);
-    })
-    .catch((e) => console.log(e));
+    if (response.ok) {
+      localStorage.setItem('userInfo', JSON.stringify(data));
+    }
+    return await response.json();
+  };
 
   const getRegistration = (e) => {
     e.preventDefault();
-    const info = new FormData(e.target);
-    if (info.get('repPassword') === info.get('password')) {
+    if (e.target[2].value === e.target[3].value) {
       setUser({
-        username: info.get('username'),
-        email: info.get('email'),
-        password: info.get('repPassword'),
+        username: e.target[0].value,
+        email: e.target[1].value,
+        password: e.target[2].value,
         loged: true,
       });
+
+      const data2 = {
+        username: e.target[0].value,
+        email: e.target[1].value,
+        password: e.target[2].value,
+        loged: true,
+      };
+
+      sendData('http://jsonplaceholder.typicode.com/users', data2).then(
+        (response) => {
+          console.log(response);
+        }
+      );
     } else {
       alert('Passwors must match!');
     }
