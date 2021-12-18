b0a52143857b398a83546e544a0a62bfde8e184a
Igoris Ivanovas <igoris.x@inbox.lt>
2021-12-18 14:50:15 +0200

    toggle drawer but style

---
+++ src/Componets/Drawer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Drawer = () => {
+  return <div className="drawer-box">aaa</div>;
+};
+
+export default Drawer;
--- src/Componets/HomePage.jsx
+++ src/Componets/HomePage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Drawer from './Drawer';
 
 const HomePage = ({ content, logout }) => {
   return (
     <div className="homePage">
       <div>
-        {JSON.stringify(content)}
-        <button onClick={logout}>Logout</button>
+        <Drawer />
+        <span className="drawer-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+        <div>{JSON.stringify(content)}</div>
       </div>
     </div>
   );
